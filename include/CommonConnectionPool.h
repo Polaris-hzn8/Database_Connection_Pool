533c8f1b189c0541301fdb694ff5097bd8fb5cf6
luochenhao <3453851623@qq.com>
2023-11-28 03:03:25 +0800

    feat: producer and consumer thread.

--- include/CommonConnectionPool.h
+++ include/CommonConnectionPool.h
@@ -13,13 +13,21 @@
 #include <mutex>
 #include <atomic>
 #include <thread>
+#include <memory>
+#include <functional>
+#include <condition_variable>
 #include "Connection.h"
 using namespace std;
 
 // 实现连接池功能模块
 class ConnectionPool {
 public:
+	// 获取连接池实例
 	static ConnectionPool* getConnectionPool();
+	// 生产新连接任务函数
+	void produceConnTask();
+	// 获取数据库连接
+	shared_ptr<Connection> getConnection();
 private:
 	ConnectionPool();
 	bool loadConfigFile();
@@ -34,10 +42,11 @@ private:
 	string _username;
 	string _password;
 	string _dbname;
-	// 数据库连接存储
+	// 数据库连接存储队列
 	queue<Connection*> _connectionq;		// 存储mysql连接的队列
 	mutex _queueMutex;						// 维护连接队列的线程安全的互斥锁
 	atomic_int _connectionCnt;				// 创建连接的数量 ++操作是线程安全的
+	condition_variable condv;					// 条件变量 用于连接生产线程和连接消费线程间的通信
 };
 
 #endif
--- src/CommonConnectionPool.cpp
+++ src/CommonConnectionPool.cpp
@@ -16,6 +16,54 @@ ConnectionPool* ConnectionPool::getConnectionPool()
     return &pool;
 }
 
+// 生产者线程的线程函数
+void ConnectionPool::produceConnTask()
+{
+	for (;;) {
+		// 队列上锁
+		unique_lock<mutex> lock(_queueMutex);
+		// 若队列不空 生产者线程进入等待状态 并且将锁释放 等待为空
+		while (!_connectionq.empty()) {
+			condv.wait(lock);
+		}
+		// 连接数量没有到达上限 则继续创建新的连接
+		if (_connectionCnt < _maxSize) {
+			Connection *p = new Connection();
+			p->connect(_ip, _port, _username, _password, _dbname);
+			_connectionq.push(p);
+			_connectionCnt++;
+		}
+		// 通知消费者线程可以消费连接了
+		condv.notify_all();
+	}
+}
+
+// 获取数据库连接
+shared_ptr<Connection> ConnectionPool::getConnection()
+{
+	// 队列上锁
+	unique_lock<mutex> lock(_queueMutex);
+	// 如果队列为空 则将锁释放 等待最大超时时间
+	if (_connectionq.empty()) {
+		// 在指定的时间之内 如果有可获取的连接 则直接将连接取走
+		condv.wait_for(lock, chrono::milliseconds(_connectionTimeout));
+		// 线程被唤醒之后队列仍然为空 则获取连接失败
+		if (_connectionq.empty()) {
+			LOG("获取空闲连接超时...");
+			return nullptr;
+		}
+	}
+	// 返回连接对象 使用智能指针管理
+	shared_ptr<Connection> sp(_connectionq.front(), [&](Connection *pcon){
+		unique_lock<mutex> lock(_queueMutex);
+		_connectionq.push(pcon);
+	});
+	_connectionq.pop();
+	// 通知生产者线程检查队列是否为空
+	condv.notify_all();
+    return sp;
+}
+
 // 初始化连接池
 ConnectionPool::ConnectionPool() {
 	// 1.加载配置项
@@ -28,6 +76,7 @@ ConnectionPool::ConnectionPool() {
 		_connectionCnt++;
 	}
 	// 3.启动一个新的线程作为连接的生产者
+	thread connProducer(std::bind(&ConnectionPool::produceConnTask, this));
 }
 
 // 读取配置文件
